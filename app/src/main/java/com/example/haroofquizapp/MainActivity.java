e14a4cbd3c1af9061b5c9cb5a4927232fa8bf9af
Chintu <bcsf18m017@pucit.edu.pk>
2021-11-09 13:19:35 +0500

    App Front Page Design

--- app/src/main/java/com/example/haroofquizapp/MainActivity.java
+++ app/src/main/java/com/example/haroofquizapp/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.haroofquizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button repo,app;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeComponents();
+        repo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://github.com/bcsf18m017/ASG_2_Haroof_Quiz");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+        app.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,AppFront.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initializeComponents() {
+        repo=(Button) findViewById(R.id.repoButton);
+        app=(Button)findViewById(R.id.appButton);
     }
 }
