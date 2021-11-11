dc12440ca02867ac90d6b534c0de1de78eca734b
Chintu <bcsf18m017@pucit.edu.pk>
2021-11-11 12:56:36 +0500

    Main,First Page,Learn Landscape view

--- app/src/main/java/com/example/haroofquizapp/ResultPage.java
+++ app/src/main/java/com/example/haroofquizapp/ResultPage.java
@@ -4,17 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.annotation.SuppressLint;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -55,6 +58,7 @@ public class ResultPage extends AppCompatActivity {
         });
 
     }
+
     private void share() {
         Bitmap bm=getBitmap(cl);
         try{
@@ -67,7 +71,7 @@ public class ResultPage extends AppCompatActivity {
             Intent intent=new Intent(Intent.ACTION_SEND);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
-            intent.setType("image/png");
+            intent.setType("image/*");
             String subject="Quiz App";
             intent.putExtra(Intent.EXTRA_TEXT,"Woo Hoo ! Here is my score\nCome Play With Me");
             intent.putExtra(Intent.EXTRA_SUBJECT, subject);
